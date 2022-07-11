d6925a7e2ac2df16a127ec5851e9dee9e7d8c1be
Monique <monique.meas@gmail.com>
2022-07-11 16:12:51 -0400

    convert fetch data into DOM element

---
+++ assets/js/single.js
@@ -0,0 +1,66 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
+var getRepoIssues = function (repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl)
+    .then(function (response) {
+        //request was successful
+        if (response.ok){
+            response.json().then(function (data){
+                // pass response date to dom function
+                displayIssues(data);
+                console.log(data);
+            });
+        }
+        else {
+            alert("There was a problem with your request!");
+        }
+    });
+    
+    console.log(repo);
+};
+
+getRepoIssues("facebook/react");
+
+var displayIssues = function(issues) {
+
+    
+
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+      }
+
+      
+
+    for (var i = 0; i < issues.length; i++) {
+        // created a link element to take issues to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justifu-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+        //create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+
+        //append to container
+        issueEl.appendChild(titleEl);
+
+        //create a type element
+
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue for a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        }
+        else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+        issueContainerEl.appendChild(issueEl);
+    }
+};
